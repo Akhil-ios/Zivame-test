c9b308ebc225c762da9d757aff9eadccc2f3f6c3
Akhil <akhil@accubits.com>
2022-04-30 20:02:01 +0530

    initial app template created

---
+++ Zivame/Zivame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zivame
+//
+//  Created by Akhil v nair on 30/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Zivame/ZivameUITests/ZivameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ZivameUITestsLaunchTests.swift
+//  ZivameUITests
+//
+//  Created by Akhil v nair on 30/04/22.
+//
+
+import XCTest
+
+class ZivameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
